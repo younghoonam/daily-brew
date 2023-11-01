79d0d3efa70f0086a22333ffd68cdedff4375cbc
Younghoo Nam <nyounghoo@gmail.com>
2023-11-01 22:05:28 +0900

    gave space between elements and added snap scrolling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { Rhino3dmLoader } from 'three/addons/loaders/3DMLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 const canvas = document.getElementById('c');
 
@@ -17,7 +17,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor('antiquewhite');
 
-const collisionBoxGeometry = new THREE.BoxGeometry( 4, 6, 1 );
+//collsiion box dimension and geometry for visualizing collision boundaries
+var collisionBoxWidth = window.innerWidth / 100;
+var collisionBoxHeight = window.innerHeight / 140;
+
+const collisionBoxGeometry = new THREE.BoxGeometry( collisionBoxWidth, collisionBoxHeight, 1 );
 const material = new THREE.MeshBasicMaterial( {
 
 	color: 0xffffff,
@@ -25,35 +29,56 @@ const material = new THREE.MeshBasicMaterial( {
 
 } );
 
+// const collisionBox = new THREE.Mesh( collisionBoxGeometry, material );
+// scene.add( collisionBox );
 
-const collisionBox = new THREE.Mesh( collisionBoxGeometry, material );
-scene.add( collisionBox );
+
+/*LIGHTS*/
+//Point Light
+const light = new THREE.PointLight( 0xffffff, 100, 100 );
+light.position.set( 5, 5, 5 );
+scene.add( light );
+
+//Ambient Light
+const alight = new THREE.AmbientLight( 0xdddddd , 1.8); // soft white light
+scene.add( alight );
+
+
+
+
 
 class bouncingObject {
-	constructor(){
-		this.geometry = new THREE.SphereGeometry(1);
-		this.material = new THREE.MeshBasicMaterial({color: 0xffa500});
-		this.mesh = new THREE.Mesh(this.geometry, this.material);
+	
+	constructor(){}
 
-		this.velocity = new THREE.Vector3(0.01,0.007,0);
-	}
+	//Default Values
+	geometry = new THREE.SphereGeometry(1);
+	material = new THREE.MeshToonMaterial({color: 0xffffff});
+	mesh = new THREE.Mesh(this.geometry, this.material);
+	velocity = new THREE.Vector3(0,0,0);
+	scale = 1;
+	xRotation = 0.01;
+	yRotation = 0.01;
 
+
+
+	//Animation Methods
 	move(){
 		this.mesh.position.x += this.velocity.x;
 		this.mesh.position.y += this.velocity.y;	
 	}
 
 	collisionCheck(){
-		if(this.mesh.position.x < -2){
+		if(this.mesh.position.x < -1 * collisionBoxWidth/2){
 			return 1;
 		}
-		if(this.mesh.position.x > 2){
+		if(this.mesh.position.x > collisionBoxWidth/2){
 			return 1;
 		}
-		if(this.mesh.position.y < -3){
+		if(this.mesh.position.y < -1 * collisionBoxHeight/2){
 			return 2;
 		}
-		if(this.mesh.position.y > 3){
+		if(this.mesh.position.y > -1 * collisionBoxHeight/2){
 			return 2;
 		}
 		else{
@@ -62,50 +87,130 @@ class bouncingObject {
 	}
 }
 
-const bouncingMug = new bouncingObject();
-var mug;
 
-// Instantiate a loader
-const loader = new Rhino3dmLoader();
-	
-// Specify path to a folder containing WASM/JS libraries or a CDN.
-// For example, /jsm/libs/rhino3dm/ is the location of the library inside the three.js repository
-// loader.setLibraryPath( '/path_to_library/rhino3dm/' );
-loader.setLibraryPath( 'https://unpkg.com/rhino3dm@8.0.0-beta2/' );
-	
-// Load a 3DM file
+/*
+mug01
+color: 0xb6e2d3 (Mint)
+velocity: 0.01, 0.007, 0
+z-position: 0
+
+mug02
+color: 0xef7c8e (IndianRed)
+velocity: -0.004 ,0.003, 0
+z-position: -5
+
+mug03
+color: 0xfae8e0 (LightPink)
+velocity: 0.01, 0.007, 0
+z-position: 0
+*/
+
+const bouncingMugs = [];
+for(let i = 0; i < 3; i++){
+	bouncingMugs.push(new bouncingObject());
+}
+
+bouncingMugs[0].material.color = new THREE.Color( 0xb6e2d3 );
+bouncingMugs[0].mesh.position.x = 2;
+bouncingMugs[0].mesh.position.y = 1;
+bouncingMugs[0].mesh.position.z = 0;
+bouncingMugs[0].velocity = new THREE.Vector3(-0.004,0.003,0);
+bouncingMugs[0].scale = 0.1;
+bouncingMugs[0].xRotation = 0.01;
+bouncingMugs[0].yRotation = 0.01;
+
+bouncingMugs[1].material.color = new THREE.Color( 0xef7c8e );
+bouncingMugs[1].mesh.position.x = -1;
+bouncingMugs[1].mesh.position.y = -1.5;
+bouncingMugs[1].mesh.position.z = -10;
+bouncingMugs[1].velocity = new THREE.Vector3(-0.004,0.003,0);
+bouncingMugs[1].scale = 0.2;
+bouncingMugs[1].xRotation = 0.003;
+bouncingMugs[1].yRotation = 0.002;
+
+bouncingMugs[2].material.color = new THREE.Color( 0xffc0cb );
+bouncingMugs[2].mesh.position.x = -0.5;
+bouncingMugs[2].mesh.position.y = 3;
+bouncingMugs[2].mesh.position.z = -5;
+bouncingMugs[2].velocity = new THREE.Vector3(0.003,-0.005,0);
+bouncingMugs[2].scale = 0.1;
+bouncingMugs[2].xRotation = 0.0025;
+bouncingMugs[2].yRotation = 0.01;
+
+
+
+
+// instantiate a loader
+const loader = new OBJLoader();
+
+// load a resource
 loader.load(
 	// resource URL
-	'mug.3dm',
-	// called when the resource is loaded
+	'mug.obj',
+	// called when resource is loaded
 	function ( object ) {
-		object.scale.multiplyScalar(0.1);
-		mug = object.clone();
-		// scene.add( object );
-		scene.add(mug);
+
+		for(let i = 0; i < bouncingMugs.length; i++){
+			bouncingMugs[i].mesh.geometry = object.children[0].geometry.clone();
+			bouncingMugs[i].mesh.scale.multiplyScalar(bouncingMugs[i].scale);
+			scene.add(bouncingMugs[i].mesh);
+		}
+
 	}
-);
+)
+
 
 const object1 = new bouncingObject();
-scene.add(object1.mesh);
+// scene.add(object1.mesh);
 
 camera.position.z = 20;
 
+// Function to handle window resizing
+function onWindowResize(){
+	const newWidth = window.innerWidth;
+	const newHeight = window.innerHeight;
+
+	// Update the camera aspect ratio and size
+	camera.aspect = newWidth / newHeight;
+	camera.updateProjectionMatrix();
+
+	// Update collision box dimensions with window size
+	collisionBoxWidth = window.innerWidth / 100;
+	collisionBoxHeight = window.innerHeight / 140;
+
+	// Update the renderer size to match the new window size
+	renderer.setSize(newWidth, newHeight);
+}
+
+// Listen for the window resize event and call the onWindowResize function
+window.addEventListener('resize', onWindowResize, false);
+
 function animate() {
+
+	//Request a new frame
 	requestAnimationFrame( animate );
 
-	object1.move();
-	if(object1.collisionCheck() == 1){
-		object1.velocity.x *= -1;
-	} else if(object1.collisionCheck() == 2){
-		object1.velocity.y *= -1;
-	} else {
+	//Mug motion and collision detection
+	for(let i = 0; i < bouncingMugs.length; i++){
+
+		//Move
+		bouncingMugs[i].move();
+
+		//Collision Check
+		if(bouncingMugs[i].collisionCheck() == 1){
+			bouncingMugs[i].velocity.x *= -1;
+		} else if(bouncingMugs[i].collisionCheck() == 2){
+			bouncingMugs[i].velocity.y *= -1;
+		}
+
+		//Rotate
+		bouncingMugs[i].mesh.rotation.x += bouncingMugs[i].xRotation;
+		bouncingMugs[i].mesh.rotation.y += bouncingMugs[i].yRotation;
+		
+
 	}
 
-	mug.rotation.x += 0.01;
-	mug.rotation.y += 0.01;
-
-
+	//Render Scene
 	renderer.render( scene, camera );
 }
 
